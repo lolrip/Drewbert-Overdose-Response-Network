87e6faca00d5893e351b9599847ee95fd88688d0
lolrip <kirk.durbin@gmail.com>
2025-06-18 12:43:23 -0400

    Replaced Geocode w Google Maps

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
-  readonly VITE_GEOCODE_API_KEY: string
+  readonly VITE_Maps_API_KEY: string // Changed from VITE_GEOCODE_API_KEY
   readonly VITE_OPENAI_API_KEY: string
   readonly VITE_OPENAI_ASSISTANT_ID: string
 }
